9b8c0affe7b584ae9915a0d8a574a3b7c647b470
Ayan 514278 <ayanf6383@gmail.com>
2026-08-15 07:58:12 +0500

    Update set.js

--- set.js
+++ set.js
@@ -104,4 +104,9 @@ function loadHistoryData() {
         historyGrid.appendChild(div);
     });
     }
-            
+      // Resolution Logic
+function fakeRes(res) {
+    localStorage.setItem('ai_resolution', res);
+    alert('Success: Resolution set to ' + res + ' (Applied for next generation)');
+}
+
